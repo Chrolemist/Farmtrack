720e0033174ed42fe1a0608558ca1cdf9414c786
Christian Vittaniemi <crv87@outlook.com>
2024-10-03 16:02:39 +0200

    La till Description i models också.

---
+++ Farmtrack/Migrations/20241003135156_AddDescriptionToCrop.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Farmtrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToCrop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Crop",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Crop");
+        }
+    }
+}
